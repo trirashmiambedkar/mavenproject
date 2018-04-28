a79cf630821553b383f7fe9ae85d8748d21e633a
Rash <Rash@Rashmi>
2018-04-28 14:20:44 +0530

    OK

---
+++ mv/src/main/java/com/groupid/mv/com/groupid/mv/App.java
@@ -0,0 +1,15 @@
+package com.groupid.mv.com.groupid.mv;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "HelloGIT!" );
+    }
+}
