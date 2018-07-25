7d69ddc6cf938bf3ae7bf0645e17ccb6047c3e8e
Rash <Rash@Rashmi>
2018-07-25 23:57:47 +0530

    branch

--- mv/src/test/java/com/groupid/mv/AppTest.java
+++ mv/src/test/java/com/groupid/mv/AppTest.java
@@ -10,34 +10,14 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
     public AppTest( String testName )
     {
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
+   
+    public void bRANCH()
     {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-    
-    public void Add()
-    {
-        System.out.println("Test case passed added");
+        System.out.println("branch");
     }
 }
