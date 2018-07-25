ce8b94ecc9d250a0e98989d8049b5409d2dcfdc0
Rash <Rash@Rashmi>
2018-07-25 23:36:05 +0530

    nm,

--- mv/src/test/java/com/groupid/mv/AppTest.java
+++ mv/src/test/java/com/groupid/mv/AppTest.java
@@ -20,27 +20,7 @@ public class AppTest
         super( testName );
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-    
-        
-    public void tc4()
-    {
-        System.out.println("Test case passed added");
-    }
+  
     
     public void NEW()
     {
